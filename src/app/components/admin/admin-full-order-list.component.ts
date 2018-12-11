669ceb736ee36ed751c8f20b890b181758abf17b
zyuiop <zyuiop@zyuiop.net>
2018-12-11 22:22:34 +0100

    Save preferences for order list

--- src/app/components/admin/admin-full-order-list.component.ts
+++ src/app/components/admin/admin-full-order-list.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {FullOrderData} from '../../types/order';
 
 @Component({
   selector: 'app-admin-full-order-list',
   templateUrl: './admin-full-order-list.component.html'
 })
-export class AdminFullOrderListComponent {
+export class AdminFullOrderListComponent implements OnInit, OnDestroy {
   @Input() orders: FullOrderData[];
   @Input() routerLinkBase: string[] = [];
   displayRemoved: boolean = false;
@@ -40,5 +40,19 @@ export class AdminFullOrderListComponent {
     const date = new Date(time);
 
     return date.toLocaleString();
+  }
+
+  ngOnDestroy(): void {
+    localStorage.setItem('preferences.displayRemoved', this.displayRemoved ? 'true' : 'false');
+    localStorage.setItem('preferences.displayUnpaid', this.displayUnpaid ? 'true' : 'false');
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('preferences.displayRemoved') === 'true') {
+      this.displayRemoved = true;
+    }
+    if (localStorage.getItem('preferences.displayUnpaid') === 'false') {
+      this.displayUnpaid = false;
+    }
   }
 }
