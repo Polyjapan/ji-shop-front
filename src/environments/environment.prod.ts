d7e784cc24da08857e7f51f11c7e88b449735cbb
zyuiop <zyuiop@zyuiop.net>
2019-05-01 01:24:44 +0200

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,10 @@ export const environment = {
   tokenWhitelist: ['shop.japan-impact.ch'],
   sumUpKey: '55e6df95-c448-495c-8c34-71702835d02c',
   sumUpApp: 'japan_impact',
-  baseUrl: 'https://shop.japan-impact.ch'
+  baseUrl: 'https://shop.japan-impact.ch',
+
+  auth: {
+    apiurl: 'https://auth.japan-impact.ch/',
+    clientId: '8pdszcUWxht75pmttKnyAxxHzc9HarutYvScP6uHq5WsDXJqGfQcWeCRHGZgHwtGE9dQvKgu'
+  },
 };
