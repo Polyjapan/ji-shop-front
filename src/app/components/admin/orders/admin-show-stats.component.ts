b2f1a9e4f36da6dbc83a67d177567b5fb7cf4be6
zyuiop <zyuiop@zyuiop.net>
2019-10-24 00:05:07 +0200

    Improve stats display

--- src/app/components/admin/orders/admin-show-stats.component.ts
+++ src/app/components/admin/orders/admin-show-stats.component.ts
@@ -3,27 +3,41 @@ import {BackendService} from '../../../services/backend.service';
 import {ActivatedRoute} from '@angular/router';
 import {SalesData, StatsReturn} from '../../../types/stats';
 import {Source} from '../../../types/order';
+import {hasOwnProperty} from 'tslint/lib/utils';
 
 @Component({
   selector: 'app-admin-show-stats',
-  templateUrl: './admin-show-stats.component.html'
+  templateUrl: './admin-show-stats.component.html',
+  styles: [
+      `
+          .total-line {
+              border-top: 3px solid black;
+              border-bottom: 3px solid black;
+              font-weight: bolder;
+          }
+    `
+  ]
 })
 export class AdminShowStatsComponent implements OnInit {
-  private id: number;
   stats: StatsReturn[];
-  totalAmountForSource: Map<string, number> = new Map();
-  totalItemsForSource: Map<string, number> = new Map();
-  cashAmount: number;
-  cardAmount: number;
-  Source = Source;
 
+  Source = Source;
+  columns: { type: Source, title: string }[] = [];
   startDate: string;
   endDate: string;
-
   effectiveStart: Date;
   effectiveEnd: Date;
+  private id: number;
 
   constructor(private backend: BackendService, private route: ActivatedRoute) {
+  }
+
+  get ticketStats() {
+    return this.stats.filter((value) => value.product.isTicket);
+  }
+
+  get goodiesStats() {
+    return this.stats.filter((value) => !value.product.isTicket);
   }
 
   ngOnInit(): void {
@@ -35,127 +49,138 @@ export class AdminShowStatsComponent implements OnInit {
 
   }
 
+  computeTotal(type: 'goodies' | 'tickets' | 'all', source?: Source) {
+    let cash = 0;
+    let card = 0;
+    let total = 0;
+    let items = 0;
+
+    for (const line of this.stats) {
+      if (type === 'all' || (type === 'tickets' && line.product.isTicket) || (type === 'goodies' && !line.product.isTicket)) {
+        if (source) {
+          if (line.salesData[source]) {
+            const value = line.salesData[source];
+            total += value.moneyGenerated;
+            if (value.moneyGeneratedCash) cash += value.moneyGeneratedCash;
+            if (value.moneyGeneratedCard) card += value.moneyGeneratedCard;
+            items += value.amountSold;
+
+            console.log(card + ' - ' + cash);
+          }
+        } else {
+          for (const k in line.salesData) {
+            if (hasOwnProperty(line.salesData, k)) {
+              const value = line.salesData[k];
+              total += value.moneyGenerated;
+              if (value.moneyGeneratedCash) cash += value.moneyGeneratedCash;
+              if (value.moneyGeneratedCard) card += value.moneyGeneratedCard;
+              items += value.amountSold;
+            }
+          }
+        }
+      }
+    }
+
+    return this.formatLine(cash, card, total, items);
+  }
+
+  getCol(source: Source, item: StatsReturn) {
+    const value: SalesData = item.salesData[source];
+
+    if (value) {
+      return this.formatLine(value.moneyGeneratedCash, value.moneyGeneratedCard, value.moneyGenerated, value.amountSold);
+    } else {
+      return this.formatLine(0, 0, 0, 0);
+    }
+  }
+
+  getTotal(item: StatsReturn) {
+    let cash = 0;
+    let card = 0;
+    let total = 0;
+    let items = 0;
+
+    for (const k in item.salesData) {
+      if (hasOwnProperty(item.salesData, k)) {
+        const value = item.salesData[k];
+        total += value.moneyGenerated;
+        cash += value.moneyGeneratedCash;
+        card += value.moneyGeneratedCard;
+        items += value.amountSold;
+      }
+    }
+
+    return this.formatLine(cash, card, total, items);
+  }
+
+  updateFilter() {
+    console.log(this.effectiveEnd);
+
+    this.effectiveStart = undefined;
+    this.effectiveEnd = undefined;
+    let changed = false;
+
+    console.log('Heyyyy i am updating yo ' + this.startDate + ' ' + this.endDate);
+
+    if (this.startDate) {
+      this.effectiveStart = this.parseTime(this.startDate);
+      changed = true;
+    }
+
+    if (this.endDate) {
+      this.effectiveEnd = this.parseTime(this.endDate);
+      console.log(this.effectiveEnd);
+      changed = true;
+    }
+
+    if (changed) {
+      this.reload();
+    }
+  }
+
   private reload() {
     this.stats = undefined;
-    this.totalItemsForSource.clear();
-    this.totalAmountForSource.clear();
-    this.cashAmount = undefined;
-    this.cardAmount = undefined;
+    this.columns = [];
 
     this.backend.getStats(this.id, this.effectiveStart, this.effectiveEnd).subscribe(stats => {
       this.stats = stats;
-      console.log(stats);
 
-      this.computeCashCard();
+      this.checkStatsFor(Source.Web, stats, 'En ligne');
+      this.checkStatsFor(Source.OnSite, stats, 'Sur place');
+      this.checkStatsFor(Source.Gift, stats, 'Cadeau');
+      this.checkStatsFor(Source.Reseller, stats, 'Import');
+      this.checkStatsFor(Source.Physical, stats, 'Physiques');
     });
   }
 
-  priceStats(data: SalesData) {
-    if (data) {
-      return data.moneyGenerated + ' CHF (' + data.amountSold + ')';
-    } else {
-      return '0 CHF (0)';
-    }
+  private formatCurrency(n) {
+    return (new Intl.NumberFormat('fr-CH', {
+      style: 'currency',
+      currency: 'CHF',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    }).format(n));
   }
 
-  priceTotalForSource(source?: string) {
-    this.computePriceTotalForSource(source);
-
-    if (!source) {
-      source = '__global';
-    }
-
-    return this.totalAmountForSource.get(source) + '  CHF (' + this.totalItemsForSource.get(source) + ')';
+  private formatNum(n) {
+    return (new Intl.NumberFormat('fr-CH').format(n));
   }
 
-  shouldDisplay(source: string): boolean {
-    this.computePriceTotalForSource(source);
+  private formatLine(cash, card, total, items) {
+    let str = this.formatNum(items) + '\t (' + this.formatCurrency(total) + ')';
+    if (cash > 0) str += '<br><i>Cash: ' + this.formatCurrency(cash) + '</i>';
+    if (card > 0) str += '<br><i>Carte: ' + this.formatCurrency(card) + '</i>';
 
-    return this.totalItemsForSource.get(source) > 0;
+    return str;
   }
 
-  paymentMethodSalesData(salesData: SalesData) {
-    const cash = salesData ? salesData.moneyGeneratedCash : undefined;
-    const card = salesData ? salesData.moneyGeneratedCard : undefined;
-
-    return this.paymentMethodData(cash, card);
-  }
-
-  paymentMethodData(cash?: number, card?: number) {
-    cash = cash ? cash : 0;
-    card = card ? card : 0;
-
-    return 'Cash: ' + cash + ' CHF, card: ' + card + ' CHF';
-  }
-
-  private computeCashCard() {
-    if (this.cardAmount && this.cashAmount) {
-      return;
-    }
-
-    let card = 0, cash = 0;
-
-
-    for (const stat of this.stats) {
-      const data = stat.salesData;
-      for (const key in data) {
-        if (data[key]) {
-          if (data[key].moneyGeneratedCard) {
-            card += data[key].moneyGeneratedCard;
-          }
-
-          if (data[key].moneyGeneratedCash) {
-            cash += data[key].moneyGeneratedCash;
-          }
-        }
+  private checkStatsFor(source: Source, stats: StatsReturn[], colTitle: string) {
+    for (const line of stats) {
+      if (line.salesData[source]) {
+        this.columns.push({type: source, title: colTitle});
+        return;
       }
     }
-
-    this.cardAmount = card;
-    this.cashAmount = cash;
-  }
-
-  private computePriceTotalForSource(source?: string) {
-    const src = source ? source : '__global';
-
-    if (this.totalAmountForSource.has(src) && this.totalItemsForSource.has(src)) {
-      return;
-    }
-
-    let total = 0;
-    let money = 0;
-
-    for (const stat of this.stats) {
-      if (source) {
-        const data = stat.salesData[source];
-        if (data) {
-          total += data.amountSold;
-          money += data.moneyGenerated;
-        }
-      } else {
-        const data = stat.salesData;
-        for (const key in data) {
-          total += data[key].amountSold;
-          money += data[key].moneyGenerated;
-        }
-      }
-
-    }
-
-    this.totalAmountForSource.set(src, money);
-    this.totalItemsForSource.set(src, total);
-  }
-
-  priceTotal(data: Map<Source, SalesData>) {
-    let amt = 0;
-    let money = 0;
-
-    for (const key in data) {
-      amt += data[key].amountSold;
-      money += data[key].moneyGenerated;
-    }
-
-    return amt + ' (' + money + ' CHF)';
   }
 
   private parseTime(str: string) {
@@ -188,30 +213,5 @@ export class AdminShowStatsComponent implements OnInit {
 
     return new Date(year, month - 1, day, hour, minute, second);
 
-  }
-
-  updateFilter() {
-    console.log(this.effectiveEnd);
-
-    this.effectiveStart = undefined;
-    this.effectiveEnd = undefined;
-    let changed = false;
-
-    console.log("Heyyyy i am updating yo " + this.startDate + " " + this.endDate);
-
-    if (this.startDate) {
-      this.effectiveStart = this.parseTime(this.startDate);
-      changed = true;
-    }
-
-    if (this.endDate) {
-      this.effectiveEnd = this.parseTime(this.endDate);
-      console.log(this.effectiveEnd);
-      changed = true;
-    }
-
-    if (changed) {
-      this.reload();
-    }
   }
 }
