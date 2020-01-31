272694cf3729fbde05000a3f27fd83cecf68e8c6
Louis Vialar <louis.vialar@gmail.com>
2020-01-31 21:16:10 +0100

    Add autocomplete attributes to authenticate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {ErrorHandler, Injectable, Injector, NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {BackendService} from './services/backend.service';
-import {AuthModule, tokenGetter} from './services/auth.module';
-import {HttpClientModule} from '@angular/common/http';
+import {AuthModule} from './services/auth.module';
+import {HTTP_INTERCEPTORS, HttpClientModule, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {HomeModule} from './components/home/home.module';
 import {AuthenticateComponent} from './components/authenticate/authenticate.component';
 import {CheckoutComponent} from './components/checkout/checkout.component';
-import {JwtHelperService, JwtModule} from '@auth0/angular-jwt';
-import {environment} from '../environments/environment';
 import {CallbackComponent} from './components/checkout/callback.component';
 import {EmailcheckComponent} from './components/authenticate/emailcheck.component';
 import {MyOrdersComponent} from './components/members/my-orders.component';
@@ -24,13 +22,51 @@ import {PermissionAuthGuard} from './services/permission-auth-guard.service';
 import {AdminModule} from './components/admin/admin.module';
 import {PosModule} from './components/pos/pos.module';
 import {RecaptchaModule} from 'ng-recaptcha';
-import {OrderContentComponent} from './components/members/order-content.component';
 import {OrderContentModule} from './components/members/order-content.module';
 import {IntranetModule} from './components/intranet/intranet.module';
 import {MainComponent} from './main.component';
 import {EmailCallbackComponent} from './components/authenticate/email-callback.component';
 import {FirstloginComponent} from './components/authenticate/firstlogin.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import * as Sentry from '@sentry/browser';
+import {Severity} from '@sentry/browser';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+Sentry.init({
+  dsn: 'https://2cb1ffaf5a934ef9b91661ecd6bcd8c7@sentry.io/2124798'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {
+  }
+
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({eventId});
+  }
+}
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private _injector: Injector,
+  ) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req)
+      .pipe(
+        tap(event => {}, error => {
+          if (error && error.error) {
+            console.log(error);
+            Sentry.captureMessage('HTTP Request failed: ' + req.method + ' ' + req.url + ' => ' + error.status + ' ' + error.message, Severity.Error);
+          }
+        })
+      );
+  }
+}
 
 @NgModule({
   declarations: [
@@ -66,6 +102,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     AppRoutingModule
   ],
   providers: [
+    {provide: ErrorHandler, useClass: SentryErrorHandler},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     BackendService,
     PermissionAuthGuard,
   ],
